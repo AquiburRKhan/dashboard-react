f6b859320dd16e822362a415bda5d18c222c58b6
AquiburRKhan <ark.aquib17@gmail.com>
2018-10-02 13:43:16 +0600

    change: upgaded to react-scripts 2.0.3

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 import createSagaMiddleware from 'redux-saga'
 import registerServiceWorker from './registerServiceWorker';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import reducers from './redux/reducers/index';
 import './utils/rehive';
 import App from './App';
@@ -16,8 +18,15 @@ const sagaMiddleware = createSagaMiddleware();
 
 const createStoreWithMiddleware = applyMiddleware(sagaMiddleware)(createStore);
 
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={createStoreWithMiddleware(persistedReducer)}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
