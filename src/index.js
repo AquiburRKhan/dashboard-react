9fd846e3f1a4cbdbd08d76b9b6810c5bdcf1ef83
AquiburRKhan <ark.aquib17@gmail.com>
2018-10-03 23:20:48 +0600

    add: added redux persist

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 import createSagaMiddleware from 'redux-saga'
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './redux/reducers/index';
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-import reducers from './redux/reducers/index';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import './utils/rehive';
 import App from './App';
 
@@ -21,15 +23,22 @@ const createStoreWithMiddleware = applyMiddleware(sagaMiddleware)(createStore);
 const persistConfig = {
     key: 'root',
     storage,
+    stateReconciler: autoMergeLevel2
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
+const store = createStoreWithMiddleware(persistedReducer);
+
+const persistor = persistStore(store);
+
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(persistedReducer)}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </PersistGate>
     </Provider>, document.getElementById('root'));
 registerServiceWorker();
 
