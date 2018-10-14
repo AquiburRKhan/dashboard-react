dd0a3ffeaea86b8cf09821205d53ae67b4b335a1
AquiburRKhan <ark.aquib17@gmail.com>
2018-10-14 22:21:41 +0600

    add: adding custom environment variables

--- src/utils/rehive.js
+++ src/utils/rehive.js
@@ -1,3 +1,13 @@
 import Rehive from 'rehive';
 
-export const r = new Rehive({ apiVersion: 3, storageMethod: 'local' });
+let r = {};
+
+if(process.env.REACT_APP_SDK_ENV === 'normal'){
+    r = new Rehive({ apiVersion: 3, storageMethod: 'local' });
+} else if(process.env.REACT_APP_SDK_ENV === 'staging'){
+    r = new Rehive({ apiVersion: 3, storageMethod: 'local', network: 'staging'});
+} else {
+    r = new Rehive({apiVersion: 3, storageMethod: 'local'});
+}
+
+export { r };
